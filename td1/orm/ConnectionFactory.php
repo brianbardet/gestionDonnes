23eab6a05cb3f5bceb61a591aa7e722316c2fe5e
BARDET Brian <brian.bardet0012@gmail.com>
2020-10-16 11:29:39 +0200

    Exercice 2 connectionFactory

---
+++ td1/orm/ConnectionFactory.php
@@ -0,0 +1,39 @@
+<?php
+namespace td1;
+require_once 'vendor/autoload.php';
+
+class ConnectionFactory {
+
+    /**
+     * Connection à la db
+     */
+    protected static $connection;
+
+    /**
+     * Création de la connection à la db
+     * @param Array conf configuration
+     */
+    public static function makeConnection(array $conf){
+
+        $user = $conf['user'];
+        $pass = $conf['pass'];
+        $name = $conf['name'];
+        $host = $conf['host'];
+        $type = $conf['type'];
+
+        try {
+            $dsn = "$type:dbname=$name;host=$host";
+            $this->connection = new PDO($dsn, $user, $pass,array(
+                PDO::ATTR_PERSISTENT => true
+            ));
+        } catch (PDOException $e) {
+            echo 'Connexion échouée : ' . $e->getMessage();
+        }
+        
+    }
+
+    public static function getConnection(){
+        return $this->connection;
+    }
+
+}
